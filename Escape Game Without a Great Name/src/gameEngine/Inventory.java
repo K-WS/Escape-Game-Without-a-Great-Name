2e6f280f0d8f75d18a9e2f0dad0a9753a9e8295f
Yootah <juta.mae13@gmail.com>
2016-05-08 20:37:09 +0300

    Credits to KW-S (tecnical problems)

--- Escape Game Without a Great Name/src/gameEngine/Inventory.java
+++ Escape Game Without a Great Name/src/gameEngine/Inventory.java
@@ -3,13 +3,25 @@ package gameEngine;
 import roomStructure.DynamicProp;
 import roomStructure.Prop;
 
+@SuppressWarnings("unused")
 public class Inventory implements Clickable{
 	private DynamicProp[] slots;
 	private int[] stack;
 	
-	public void add(Prop item){}
+	//oletan, et slots on hoiukoht objektidele praegu
+	//selle põhjal ehitan hetkel inventory
+	
+	public void add(Prop item){
+		for (int i = 0; i<slots.length; i++){
+			if(slots[i].getID == null){slots[i] = item;}
+		}
+		
+	}
 	public void remove(int index){}
-	public void displayDescription(int index){}
+	public void displayDescription(int index){
+		//oletuslikviis selgituse saamiseks
+		System.out.println(slots[index].getDescription());
+	}
 	
 	public void React(){}
 
--- Escape Game Without a Great Name/src/objectCreation/FileParser.java
+++ Escape Game Without a Great Name/src/objectCreation/FileParser.java
@@ -2,6 +2,9 @@ package objectCreation;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.Scanner;
+
+import roomStructure.Prop;
 
 public class FileParser{
 	private HashMap<String, File> dataFiles;
@@ -21,7 +24,20 @@ public class FileParser{
 		return null;
 		}
 	public String[] parseXProps(int x){return null;}
-	public String[] parseAllProps() { return null; }
+	
+	public HashMap<String, Prop> parseAllProps() throws Exception{
+		//string - objekti id
+		//prop - objekt selles ruumis
+		
+		Scanner sc = new Scanner(dataFiles.get("props"));
+		while(sc.hasNextLine()){
+			System.out.println(sc.nextLine());
+		}
+		
+		sc.close();
+		return null;
+	
+	}
 	public File loadPictureFromFile(String filename){return null;}
 
 	
