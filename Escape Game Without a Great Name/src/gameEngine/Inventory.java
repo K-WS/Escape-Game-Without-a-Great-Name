0faf392d0ecc385544f1b7165c24d4a7209be556
Yootah <juta.mae13@gmail.com>
2016-05-09 16:38:42 +0300

    afjasfjlasff

--- Escape Game Without a Great Name/src/gameEngine/Inventory.java
+++ Escape Game Without a Great Name/src/gameEngine/Inventory.java
@@ -3,7 +3,6 @@ package gameEngine;
 import roomStructure.DynamicProp;
 import roomStructure.Prop;
 
-@SuppressWarnings("unused")
 public class Inventory implements Clickable{
 	private DynamicProp[] slots;
 	private int[] stack;
@@ -13,7 +12,7 @@ public class Inventory implements Clickable{
 	
 	public void add(Prop item){
 		for (int i = 0; i<slots.length; i++){
-			if(slots[i].getID == null){slots[i] = item;}
+			//if(slots[i].getID == null){slots[i] = item;}
 		}
 		
 	}
@@ -22,7 +21,7 @@ public class Inventory implements Clickable{
 	}
 	public void displayDescription(int index){
 		//oletuslikviis selgituse saamiseks
-		System.out.println(slots[index].getDescription());
+		//System.out.println(slots[index].getDescription());
 	}
 	
 	public void React(){}
